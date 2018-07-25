eb7ff940d71f2d7cc7ded8b880c2683a3e05b640
Brendan Morrell <brendanmorrell@gmail.com>
2018-07-25 11:04:56 -0700

    working on this shit with resolving sass partials

--- src/store/configure.js
+++ src/store/configure.js
@@ -2,9 +2,9 @@ import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
-import moviesReducer from '../reducers/moviesReducer';
-import usersReducer from '../reducers/usersReducer';
-import filtersReducer from '../reducers/filtersReducer';
+import moviesReducer from '../reducers/moviesReducer.js';
+import usersReducer from '../reducers/usersReducer.js';
+import filtersReducer from '../reducers/filtersReducer.js';
 import historyReducer from '../reducers/historyReducer.js';
 
 export default () => {
